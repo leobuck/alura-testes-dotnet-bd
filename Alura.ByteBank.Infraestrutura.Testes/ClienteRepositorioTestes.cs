9d906d4f0863eb1c0011a0a766a40dc80be10714
leobuck <leobuck12@gmail.com>
2023-07-27 21:08:05 -0300

    Testando os selects

--- Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
+++ Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -31,5 +31,29 @@ namespace Alura.ByteBank.Infraestrutura.Testes
 			Assert.NotNull(lista);
 			Assert.Equal(3, lista.Count);
 		}
+
+		[Fact]
+		public void TestaObterClientePorId()
+		{
+			// Arrange
+			// Act
+			var cliente = _repositorio.ObterPorId(1);
+
+			// Assert
+			Assert.NotNull(cliente);
+		}
+
+		[Theory]
+		[InlineData(1)]
+		[InlineData(2)]
+		public void TestaObterClientesPorVariosId(int id)
+		{
+			// Arrange
+			// Act
+			var cliente = _repositorio.ObterPorId(id);
+
+			// Assert
+			Assert.NotNull(cliente);
+		}
 	}
 }
