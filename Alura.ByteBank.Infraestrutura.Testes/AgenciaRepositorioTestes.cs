7da7c429eb5161f354453b3e3f2b0ed09e17875c
leobuck <leobuck12@gmail.com>
2023-07-27 21:52:12 -0300

    Testando as exceções

---
+++ Alura.ByteBank.Infraestrutura.Testes/AgenciaRepositorioTestes.cs
@@ -0,0 +1,35 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+	public class AgenciaRepositorioTestes
+	{
+		private readonly IAgenciaRepositorio _repositorio;
+
+        public AgenciaRepositorioTestes()
+        {
+			var servico = new ServiceCollection();
+			servico.AddTransient<IAgenciaRepositorio, AgenciaRepositorio>();
+			var provedor = servico.BuildServiceProvider();
+			_repositorio = provedor.GetService<IAgenciaRepositorio>();
+		}
+
+		[Fact]
+		public void TestaExcecaoConsultaAgenciaPorId()
+		{
+			// Act
+			// Assert
+			Assert.Throws<FormatException>(
+				() => _repositorio.ObterPorId(33)
+			);
+		}
+    }
+}
--- Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
+++ Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
@@ -4,9 +4,6 @@ using Alura.ByteBank.Dominio.Interfaces.Repositorios;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Alura.ByteBank.Infraestrutura.Testes
